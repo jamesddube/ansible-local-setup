f0cc5be25b54d0b8726493f834a5dbc438c6ed1b
James Dube <jamesdylandube@gmail.com>
2020-03-20 08:23:14 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+NC='\033[0m'
+
+echo -e "${BYellow}Installing dependencies ${NC}"
+sudo apt update && sudo apt install -y git python-pip python-dev
+echo -e "${BGreen}done ${NC}"
+echo -e "${BYellow}Downloading and installing ansible ${NC}"
+sudo pip install --upgrade ansible
+echo -e "${BGreen}done ${NC}"
+
+echo -e "${BYellow}Cloning playbook repository ${NC}"
+git clone git@github.com:jamesddube/ansible-local-setup.git ~/ansible-local-setup
+cd ~/ansible-local-setup
+echo -e "${BYellow}Let the games begin... ${NC}"
+git checkout -b development
+ansible-playbook playbooks/setup.yml -i ./hosts
